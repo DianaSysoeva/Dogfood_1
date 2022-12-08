297feea94188fd44ad3622e4cc7d14446972a307
Sysoeva-Diana <pervakova.diana@gmail.com>
2022-12-08 14:00:22 +0300

    Added update version with routing for gh-pages.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import { ProductPage } from './pages/ProductPage/product-page';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';//заменила для отображения на gh-pages.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<BrowserRouter>
+	<HashRouter>
 		<App />
-	</BrowserRouter>
+	</HashRouter>
 
 );
 
